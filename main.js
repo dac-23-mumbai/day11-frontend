c1d7b6fd74b092f53650fb0cd885471c7f1e8345
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-11 09:34:54 +0530

    fronte end integration.

--- main.js
+++ main.js
@@ -10,4 +10,24 @@ function addRecord() {
 
   // clear the box
   inputMessageRef.value = "";
+
+  // refresh the page
+  readOrFindAllRecord();
+}
+
+async function readOrFindAllRecord() {
+  let uri = "http://localhost:4000/findAll";
+
+  let res = await fetch(uri);
+  let list = await res.json();
+  console.log(list);
+
+  // iternateion
+  for (let item of list) {
+    let parentRef = document.querySelector("#parent");
+    let existingValue = parentRef.innerHTML;
+    let newValue = `<h1>${item.message}</h1>`;
+
+    parentRef.innerHTML = newValue + existingValue;
+  }
 }
