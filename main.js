a98592ab9d1f4681db17a5910f896621bdea5105
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-11 09:22:18 +0530

    with input.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 function addRecord() {
-  let uri = `http://localhost:4000/addrecord?message=hiiiiiiiii`;
+  let inputMessageRef = document.querySelector("#messageId");
+  let message = inputMessageRef.value;
+
+  let uri = `http://localhost:4000/addrecord?message=${message}`;
 
   // connect the backend, ajax
+  // magic, star, superstar
   fetch(uri);
+
+  // clear the box
+  inputMessageRef.value = "";
 }
